ff829fb5967b6ceedc591978dbfa0391d345a0a3
Vlad Chirila <vlad.chirila@live.com>
2023-01-17 19:03:27 +0200

    Add start, stop, reset buttons and logo

--- Pomodoro/MainPage.xaml.cs
+++ Pomodoro/MainPage.xaml.cs
@@ -1,24 +1,46 @@
 ﻿namespace Pomodoro;
+using System.Timers;
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
+    private readonly Timer _timer;
+    private long _remainingTime = 0;
+    private long _workInterval = 25; //seconds
 
-	public MainPage()
-	{
-		InitializeComponent();
-	}
+    public MainPage()
+    {
+        InitializeComponent();
+        _timer = new Timer(1000);
+        _timer.Elapsed += OnTimedEvent;
+        _timer.AutoReset = true;
+        _timer.Enabled = false;
+    }
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
+    private void OnTimedEvent(object source, ElapsedEventArgs e)
+    {
+        // Update UI with the remaining time
+        Device.BeginInvokeOnMainThread(() =>
+        {
+            RemainingTimeLabel.Text = _remainingTime.ToString();
+        });
+        _remainingTime--;
 
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
+        if(_remainingTime == 0)
+        {
+            _timer.Stop();
+            // TODO: Send notification
+        }
+    }
 
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
+    private void OnStartButtonClicked(object source, EventArgs e) {
+        _timer.Start();
+    }
+    private void OnStopButtonClicked(object source, EventArgs e) {
+        _timer.Stop();
+    }
+    private void OnResetButtonClicked(object source, EventArgs e) {
+        _timer.Stop();
+        _remainingTime = _workInterval;
+    }
 }
 
