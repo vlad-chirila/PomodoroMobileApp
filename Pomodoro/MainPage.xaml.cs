e5ca122dddbbf132b81ea4cdb86f0da7cf604d8d
Vlad Chirila <vlad.chirila@live.com>
2023-02-04 15:03:34 +0200

    .

--- Pomodoro/MainPage.xaml.cs
+++ Pomodoro/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿namespace Pomodoro;
+
 using System.Timers;
 
 public partial class MainPage : ContentPage
@@ -29,6 +30,20 @@ public partial class MainPage : ContentPage
         {
             _timer.Stop();
             // TODO: Send notification
+            //var notification = new Notification
+            //{
+            //    Title = "Time's up!",
+            //    Message = "Have a break :)"
+
+            //};
+           // Notification.Send(notification);
+            //Save to database
+            //var task = new Task { Name = _currentTask, CompletedAt = DateTime.Now };
+            //using (var connection = new SQLLiteConnection(App.DatabasePath))
+            //{
+            //    connection.CreateTable<Task>();
+            //    connection.Insert(task);
+            //}
         }
     }
 
@@ -41,6 +56,11 @@ public partial class MainPage : ContentPage
     private void OnResetButtonClicked(object source, EventArgs e) {
         _timer.Stop();
         _remainingTime = _workInterval;
+    }
+
+    private void ViewSettingsButton_Clicked(object source, EventArgs e)
+    {
+        //Application.Current.MainPage = new SettingsPage();
     }
 }
 
